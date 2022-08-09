bc93cd3c1e1cd23fab8b2713277b256a2dbe8355
WXZ <53850249+18883234348@users.noreply.github.com>
2022-08-09 23:24:40 +0800

    Update V-pie.py

--- V-pie.py
+++ V-pie.py
@@ -9,7 +9,8 @@ bl_info = {
     "version": (0, 0, 1),
     "blender": (3, 3, 0),
     "location": "View3D",
-    "category": "3D View"}
+    "category": "3D View",
+}
 
 
 class VIEW3D_PIE_MT_Bottom_V(Menu):
@@ -17,28 +18,46 @@ class VIEW3D_PIE_MT_Bottom_V(Menu):
 
     def draw(self, context):
         layout = self.layout
-        layout.alignment = "CENTER"
         pie = layout.menu_pie()
 
         ob_type = context.object.type
         ob_mode = context.object.mode
 
-        # 4 - LEFT
-        pie.operator('curve.handle_type_set').type = 'VECTOR'
-        # 6 - RIGHT
-        pie.operator('curve.handle_type_set').type = 'ALIGNED'
-        # 2 - BOTTOM
-        pie.separator()
-        # 8 - TOP
-        pie.operator('curve.handle_type_set').type = 'TOGGLE_FREE_ALIGN'
-        # 7 - TOP - LEFT
-        pie.operator('curve.handle_type_set').type = 'FREE_ALIGN'
-        # 9 - TOP - RIGHT
-        pie.operator('curve.handle_type_set').type = 'AUTOMATIC'
-        # 1 - BOTTOM - LEFT
-        pie.separator()
-        # 3 - BOTTOM - RIGHT
-        pie.separator()
+        if ob_mode == 'OBJECT':
+            # 4 - LEFT
+            pie.separator()
+            # 6 - RIGHT
+            pie.separator()
+            # 2 - BOTTOM
+            pie.separator()
+            # 8 - TOP
+            pie.separator()
+            # 7 - TOP - LEFT
+            pie.separator()
+            # 9 - TOP - RIGHT
+            pie.separator()
+            # 1 - BOTTOM - LEFT
+            pie.separator()
+            # 3 - BOTTOM - RIGHT
+
+        elif ob_mode == 'EDIT':
+            if ob_type == 'CURVE':
+                # 4 - LEFT
+                pie.operator('curve.handle_type_set').type = 'VECTOR'
+                # 6 - RIGHT
+                pie.operator('curve.handle_type_set').type = 'ALIGNED'
+                # 2 - BOTTOM
+                pie.separator()
+                # 8 - TOP
+                pie.operator('curve.handle_type_set').type = 'TOGGLE_FREE_ALIGN'
+                # 7 - TOP - LEFT
+                pie.operator('curve.handle_type_set').type = 'FREE_ALIGN'
+                # 9 - TOP - RIGHT
+                pie.operator('curve.handle_type_set').type = 'AUTOMATIC'
+                # 1 - BOTTOM - LEFT
+                pie.separator()
+                # 3 - BOTTOM - RIGHT
+                pie.separator()
 
 
 classes = [
@@ -51,8 +70,8 @@ addon_keymaps = []
 def register_keymaps():
     addon = bpy.context.window_manager.keyconfigs.addon
 
-    km = addon.keymaps.new(name="Curve", space_type="VIEW_3D")
-    kmi = km.keymap_items.new("wm.call_menu_pie",  'V', 'CLICK_DRAG')
+    km = addon.keymaps.new(name="View 3D", space_type="VIEW_3D")
+    kmi = km.keymap_items.new("wm.call_menu_pie", 'V', 'CLICK_DRAG')
     kmi.properties.name = "VIEW3D_PIE_MT_Bottom_V"
     addon_keymaps.append(km)
 
