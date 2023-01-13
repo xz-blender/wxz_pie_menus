c27dc52764fbea025c480d508328d0fad3081b4c
WXZ <wxz18883234348@gmail.com>
2023-01-13 13:31:23 +0800

    Update enable_relay_addons.py

--- enable_relay_addons.py
+++ enable_relay_addons.py
@@ -203,7 +203,12 @@ class Enable_Pie_Menu_Relay_Addons(Operator):
                 bpy.ops.preferences.addon_disable(module = disable)
 
         # 部分插件其他设置
-        bpy.ops.simpletabs.import_settings(filepath=r"D:/OneDrive/Sync/Blender/Blender_Mapping/config/simpletabs_prefs.json")
+        # Simple Tabs
+        try:
+            bpy.ops.simpletabs.import_settings(filepath=r"D:/OneDrive/Sync/Blender/Blender_Mapping/config/simpletabs_prefs.json")
+            bpy.ops.simpletabs.update()
+        except:
+            print("Simple Tabs settings error")
 
         return {"FINISHED"}
 
