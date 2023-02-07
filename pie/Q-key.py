fbe576bcda88a9685439bd3d101562038d447aa6
WXZ <wxz18883234348@gmail.com>
2023-02-07 20:01:04 +0800

    Update Q-key.py

--- pie/Q-key.py
+++ pie/Q-key.py
@@ -165,12 +165,7 @@ addon_keymaps = []
 def register_keymaps():
     addon = bpy.context.window_manager.keyconfigs.addon
 
-    km = addon.keymaps.new(name="Object Mode")
-    kmi = km.keymap_items.new("pie.q_key", 'Q', 'CLICK')
-    kmi = km.keymap_items.new("pie.q_key_shift", 'Q', 'CLICK', shift=True)
-    addon_keymaps.append(km)
-
-    km = addon.keymaps.new(name="Mesh")
+    km = addon.keymaps.new(name="3D View", space_type = 'VIEW_3D')
     kmi = km.keymap_items.new("pie.q_key", 'Q', 'CLICK')
     kmi = km.keymap_items.new("pie.q_key_shift", 'Q', 'CLICK', shift=True)
     addon_keymaps.append(km)
